e8626d98799cfb09d6886ca8c9d6ff39a433e318
leberhartphillips <luke.eberhart@gmail.com>
2022-02-23 12:31:11 +0100

    modified flight and fledge models for sex and species

--- R/analysis/analysis_age_first_flight.R
+++ R/analysis/analysis_age_first_flight.R
@@ -19,17 +19,18 @@ flight_dat <-
                 species = Spp,
                 flight_age = days_since_fledging,
                 Ring_ID = Ind_ID) %>% 
-  mutate(sex_plot = ifelse(sex == "Male", 2.2, 0.8))
+  mutate(sex = ifelse(sex == "Male", "M", ifelse(sex == "Female", "F", sex))) %>% 
+  mutate(sex_plot = ifelse(sex == "M", 2.2, 0.8))
 
 # assess normality of "days_since_fledging" variable
-# ggplot(data = flight_dat) +
-#   geom_histogram(aes(days_since_fledging), binwidth = 3) +
-#   facet_grid(Sex ~ Spp)
+ggplot(data = flight_dat) +
+  geom_histogram(aes(flight_age), binwidth = 3) +
+  facet_grid(sex ~ species)
 
 # check for repeated measures within nest
-# flight_dat %>% 
-#   group_by(Nst_No) %>% 
-#   dplyr::summarise(n_ = n())
+flight_dat %>%
+  group_by(nest_ID) %>%
+  dplyr::summarise(n_ = n())
 
 # see here for a blog about how to bootstrap the model to estimate 
 # the 95% confidence interval around the predictions:
@@ -40,12 +41,57 @@ flight_dat <-
 # library(bootpredictlme4)
 
 #### Modeling (WBC) ----
+# subset to BC males
+male_BC <- filter(flight_dat, species == "BC" & sex == "M")
+
+# assess collinearity
+cor.test(male_BC$Fledge_tarsus, male_BC$Fledge_mass) # highly colinear: 0.48
+cor.test(male_BC$flight_age, male_BC$Fledge_mass) # 0.24
+cor.test(male_BC$flight_age, male_BC$Fledge_tarsus) # 0.45 ... better than mass
+
+mod_flight_age_BC_male <- 
+  lmer(flight_age ~ 1 +
+         (1 | nest_ID) + (1 | year), 
+       data = male_BC)
+
+# simulate prediction
+predict_BC_male <- 
+  predict(mod_flight_age_BC_male, 
+          #newdata = new_data_BC_male, 
+          re.form = NA, 
+          se.fit = TRUE, 
+          nsim = 1000)
+
+summary(mod_flight_age_BC_male)
+
+# run model
+mod_fledge_age_BC_male <- 
+  lmer(Fledge_age ~ Fledge_tarsus +
+         (1 | nest_ID) + (1 | year), 
+       data = male_BC)
+
+# get the average size of a male tarsus for the model prediction
+new_data_BC_male <- 
+  expand.grid(Fledge_tarsus = 
+                mean(male_BC$Fledge_tarsus, 
+                     na.rm = TRUE))
+
+# simulate prediction
+predict_BC_male <- 
+  predict(mod_fledge_age_BC_male, 
+          #newdata = new_data_BC_male, 
+          re.form = NA, 
+          se.fit = TRUE, 
+          nsim = 1000)
+
 # "flight_age" as dependent variable, 
 # "sex", "fledge_age", and "fledge_mass" as independent
 mod_flight_age_WBC <-
   lmer(flight_age ~ sex + Fledge_age + Fledge_mass +
          (1 | nest_ID) + (1 | year), 
        data = filter(flight_dat, species == "WBC"))
+
+model_parameters(mod_flight_age_WBC)
 
 new_data_WBC <- 
   expand.grid(sex = c("Female","Male"),
@@ -54,10 +100,29 @@ new_data_WBC <-
 
 predict_WBC <- 
   predict(mod_flight_age_WBC, 
-        newdata = new_data_WBC, 
-        re.form = NA, 
-        se.fit = TRUE, 
-        nsim = 1000)
+          newdata = new_data_WBC, 
+          re.form = NA, 
+          se.fit = TRUE, 
+          nsim = 1000)
+
+plot(allEffects(mod_flight_age_WBC))
+
+mod_flight_age_WBC2 <-
+  lmer(flight_age ~ sex + 
+         (1 | nest_ID) + (1 | year), 
+       data = filter(flight_dat, species == "WBC"))
+
+new_data_WBC2 <- 
+  expand.grid(sex = c("Female","Male"))
+              #Fledge_age = mean(flight_dat[flight_dat$species == "WBC",]$Fledge_age),
+              #Fledge_mass = mean(flight_dat[flight_dat$species == "WBC",]$Fledge_mass))
+
+predict_WBC2 <- 
+  predict(mod_flight_age_WBC2, 
+          newdata = new_data_WBC2, 
+          re.form = NA, 
+          se.fit = TRUE, 
+          nsim = 1000)
 
 # boot_WBC_flight_age <- 
 #   bootMer(mod_flight_age_WBC, nsim = 1000, 
@@ -78,10 +143,12 @@ new_data_BC <-
 
 predict_BC <- 
   predict(mod_flight_age_BC, 
-        newdata = new_data_BC, 
-        re.form = NA, 
-        se.fit = TRUE, 
-        nsim = 1000)
+          newdata = new_data_BC, 
+          re.form = NA, 
+          se.fit = TRUE, 
+          nsim = 1000)
+
+plot(allEffects(mod_flight_age_BC))
 
 # boot_BC_flight_age <- 
 #   bootMer(mod_flight_age_BC, nsim = 1000, 
--- R/analysis/analysis_fledge_age.R
+++ R/analysis/analysis_fledge_age.R
@@ -13,79 +13,182 @@ try (sapply(function.sources, source), silent = TRUE)
 fledge_dat <- 
   read.csv("data/raw/Coucal_chick_survival_2001-2019_20200129.csv", 
            header = TRUE, stringsAsFactors = FALSE, na.strings = c("", " ", "NA")) %>%
-  dplyr::select(species, sex, Fledge_age, nest_ID, year, lab_no, Ring_ID, site) %>% 
+  dplyr::select(species, sex, Fledge_age, Fledge_mass, Fledge_tarsus, nest_ID, 
+                year, lab_no, Ring_ID, site, brood_size, age_diff, lay_date) %>% 
   filter(site == "Kapunga" & species != "CTC") %>% 
   mutate(Nst_No = str_replace_all(string = nest_ID, fixed(" "), ""),
          Ring_ID = str_replace_all(string = Ring_ID, fixed(" "), ""),
          lab_no = str_replace_all(string = lab_no, fixed(" "), "")) %>% 
   filter(!is.na(Fledge_age)) %>% 
   mutate(Ind_ID = paste(Nst_No, Ring_ID, lab_no, sep = "_")) %>%
-  mutate(sex_plot = ifelse(sex == "M", 2.2, 0.8))
+  mutate(sex_plot = ifelse(sex == "M", 2.2, 0.8)) %>% 
+  mutate(
+    # scale the Julian lay date by year
+    lay_date_std = scale_by(lay_date ~ year, ., scale = 0)) %>% 
+  
+  # make the scaled date variable numeric class
+  mutate(lay_date_std_num = as.numeric(lay_date_std))
 
-# # assess normality of "days_since_fledging" variable
-# ggplot(data = fledge_dat) +
-#   geom_histogram(aes(Fledge_age), binwidth = 1) +
-#   facet_grid(sex ~ species)
+# assess normality of "days_since_fledging" variable
+ggplot(data = fledge_dat) +
+  geom_histogram(aes(Fledge_age), binwidth = 1) +
+  facet_grid(sex ~ species)
 
 # check for repeated measures within nest
-# fledge_dat %>% 
-#   group_by(nest_ID) %>% 
-#   dplyr::summarise(n_ = n())
+fledge_dat %>%
+  group_by(nest_ID) %>%
+  dplyr::summarise(n_ = n())
  
+# Modelling proceedure:
+# "Fledge_age" as dependent variable nest_ID and year as random effects.
+# Each sex was modeled seperartely to estimate the sex-specific fledge age as an 
+# input for the demographic model.
+# To control for the effect of body size on age we evaluated the effects of 
+# fledge tarsus and fledge mass on fledge age. Fledge mass and fledge tarsus were 
+# highly colinear, but fledge tarsus had a better correlation with fledge age and 
+# so fledge mass was dropped.
+# To control for seasonal effects on fledge age, we evaluated the effect of
+# standardized lay day date, however this had virtualy no effect and to make the 
+# model parsimonious, we dropped this too.
+# Final model included "Fledge_tarsus" as the lone independent variable
+
 #### Modeling (BC) ----
-# "Fledge_age" as dependent variable, interaction with sex and species
-mod_fledge_age_BC <- 
-  lmer(Fledge_age ~ sex + 
+# subset to BC males
+male_BC <- filter(fledge_dat, species == "BC" & sex == "M")
+
+# assess collinearity
+cor.test(male_BC$Fledge_tarsus, male_BC$Fledge_mass) # highly colinear: 0.63
+cor.test(male_BC$Fledge_age, male_BC$Fledge_mass) # 0.24
+cor.test(male_BC$Fledge_age, male_BC$Fledge_tarsus) # 0.45 ... better than mass
+
+# run model
+mod_fledge_age_BC_male <- 
+  lmer(Fledge_age ~ Fledge_tarsus +
          (1 | nest_ID) + (1 | year), 
-       data = filter(fledge_dat, species == "BC"))
+       data = male_BC)
 
-new_data_BC <- 
-  expand.grid(sex = c("F","M"))
+# get the average size of a male tarsus for the model prediction
+new_data_BC_male <- 
+  expand.grid(Fledge_tarsus = 
+                mean(male_BC$Fledge_tarsus, 
+                     na.rm = TRUE))
 
-predict_BC <- 
-  predict(mod_fledge_age_BC, 
-          newdata = new_data_BC, 
+# simulate prediction
+predict_BC_male <- 
+  predict(mod_fledge_age_BC_male, 
+          newdata = new_data_BC_male, 
           re.form = NA, 
           se.fit = TRUE, 
           nsim = 1000)
+
+# subset to BC females
+female_BC <- filter(fledge_dat, species == "BC" & sex == "F")
+
+# assess collinearity
+cor.test(female_BC$Fledge_tarsus, female_BC$Fledge_mass) # highly colinear: 0.70
+cor.test(female_BC$Fledge_age, female_BC$Fledge_mass) # 0.47
+cor.test(female_BC$Fledge_age, female_BC$Fledge_tarsus) # 0.60 ... better than mass
+
+# run model
+mod_fledge_age_BC_female <- 
+  lmer(Fledge_age ~ Fledge_tarsus +
+         (1 | nest_ID) + (1 | year), 
+       data = female_BC)
+
+# get the average size of a female tarsus for the model prediction
+new_data_BC_female <- 
+  expand.grid(Fledge_tarsus = 
+                mean(female_BC$Fledge_tarsus, 
+                     na.rm = TRUE))
+
+# simulate prediction
+predict_BC_female <- 
+  predict(mod_fledge_age_BC_female, 
+          newdata = new_data_BC_female, 
+          re.form = NA, 
+          se.fit = TRUE, 
+          nsim = 1000)
+
+predict_BC_male$fit[1]
+predict_BC_female$fit[1]
 
 #### Modeling (WBC) ----
-# "Fledge_age" as dependent variable, interaction with sex and species
-mod_fledge_age_WBC <- 
-  lmer(Fledge_age ~ sex + 
+# subset to WBC males
+male_WBC <- filter(fledge_dat, species == "WBC" & sex == "M")
+
+# assess collinearity
+cor.test(male_WBC$Fledge_tarsus, male_WBC$Fledge_mass) # highly colinear: 0.63
+cor.test(male_WBC$Fledge_age, male_WBC$Fledge_mass) # 0.32
+cor.test(male_WBC$Fledge_age, male_WBC$Fledge_tarsus) # 0.53 ... better than mass
+
+# run model
+mod_fledge_age_WBC_male <- 
+  lmer(Fledge_age ~ Fledge_tarsus +
          (1 | nest_ID) + (1 | year), 
-       data = filter(fledge_dat, species == "WBC"))
+       data = male_WBC)
 
-new_data_WBC <- 
-  expand.grid(sex = c("F","M"))
+# get the average size of a male tarsus for the model prediction
+new_data_WBC_male <- 
+  expand.grid(Fledge_tarsus = 
+                mean(male_WBC$Fledge_tarsus, 
+                     na.rm = TRUE))
 
-predict_WBC <- 
-  predict(mod_fledge_age_WBC, 
-          newdata = new_data_WBC, 
+# simulate prediction
+predict_WBC_male <- 
+  predict(mod_fledge_age_WBC_male, 
+          newdata = new_data_WBC_male, 
           re.form = NA, 
           se.fit = TRUE, 
           nsim = 1000)
 
-# plot effect sizes of model
-# plot(allEffects(fledge_age_mod))
+# subset to BC females
+female_WBC <- filter(fledge_dat, species == "WBC" & sex == "F")
+
+# assess collinearity
+cor.test(female_WBC$Fledge_tarsus, female_WBC$Fledge_mass) # highly colinear: 0.69
+cor.test(female_WBC$Fledge_age, female_WBC$Fledge_mass) # 0.50
+cor.test(female_WBC$Fledge_age, female_WBC$Fledge_tarsus) # 0.52 ... better than mass
+
+# run model
+mod_fledge_age_WBC_female <- 
+  lmer(Fledge_age ~ Fledge_tarsus +
+         (1 | nest_ID) + (1 | year), 
+       data = female_WBC)
+
+# get the average size of a female tarsus for the model prediction
+new_data_WBC_female <- 
+  expand.grid(Fledge_tarsus = 
+                mean(female_WBC$Fledge_tarsus, 
+                     na.rm = TRUE))
+
+# simulate prediction
+predict_WBC_female <- 
+  predict(mod_fledge_age_WBC_female, 
+          newdata = new_data_WBC_female, 
+          re.form = NA, 
+          se.fit = TRUE, 
+          nsim = 1000)
+
+predict_WBC_male$fit[1]
+predict_WBC_female$fit[1]
 
 # tidy up estimates
 coucal_fledge_age <- 
   data.frame(trait = c("fledge_age"),
              species = c("BC", "BC", "WBC", "WBC"),
              sex = c("F", "M", "F", "M"),
-             mean = c(predict_BC$fit[1],
-                      predict_BC$fit[2],
-                      predict_WBC$fit[1],
-                      predict_WBC$fit[2]),
-             CI_low = c(predict_BC$ci.fit[1, 1],
-                        predict_BC$ci.fit[1, 2],
-                        predict_WBC$ci.fit[1, 1],
-                        predict_WBC$ci.fit[1, 2]),
-             CI_high = c(predict_BC$ci.fit[2, 1],
-                         predict_BC$ci.fit[2, 2],
-                         predict_WBC$ci.fit[2, 1],
-                         predict_WBC$ci.fit[2, 2]),
+             mean = c(predict_BC_female$fit[1],
+                      predict_BC_male$fit[1],
+                      predict_WBC_female$fit[1],
+                      predict_WBC_male$fit[1]),
+             CI_low = c(predict_BC_female$ci.fit[1, 1],
+                        predict_BC_male$ci.fit[1, 1],
+                        predict_WBC_female$ci.fit[1, 1],
+                        predict_WBC_male$ci.fit[1, 1]),
+             CI_high = c(predict_BC_female$ci.fit[2, 1],
+                         predict_BC_male$ci.fit[2, 1],
+                         predict_WBC_female$ci.fit[2, 1],
+                         predict_WBC_male$ci.fit[2, 1]),
              n_inds = c(filter(fledge_dat, species == "BC" & sex == "F") %>% 
                           summarise(n_ = n_distinct(Ind_ID)) %>% 
                           pull(n_),
